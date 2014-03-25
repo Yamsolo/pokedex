2665da8d22fed8930d0aa26f3bf17a24e8add846
William BENAISSA <E139157H@I019V7pc18.ensinfo.sciences.univ-nantes.prive>
2014-03-25 18:30:21 +0100

    Promis, on va y arriver

--- connexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$link = mysql_connect("mysql.ensinfo.sciences.univ-nantes.prive", "e139157H", "45eb1f62") or die("Impossible de se connecter à la base");
-	mysql_select_db("e139157H", $link) or die("Impossible de selectionner la base de donnée");
-
-	mysql_close($link);
-?>

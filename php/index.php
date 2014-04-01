bde9853a6e702b61622d8afd97c0944d02805dc6
William BENAISSA <E139157H@I123V2pc17.ensinfo.sciences.univ-nantes.prive>
2014-04-01 18:03:37 +0200

    Mise a jour de la frontpage + gestion des query dans le table html.
    * php/index.php: ajout du select pour tri
    
    * php/connexion.php: entrée d'une liste des pokemon dans le table

--- php/index.php
+++ php/index.php
@@ -2,28 +2,37 @@
 <html>
 	<head>
 		<title>PC de Léo</title>
-        <link rel=stylesheet href="../css/main.css" type="text/css">
+        	<link rel=stylesheet href="../css/main.css" type="text/css">
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-		<?php include("connexion.php"); ?>
-		<table border=2>
+		<h1>liste de pokemons</h1>
+		<form method="post" action="">
+			<select id="tri_select">
+				<option name="nom">Nom</option>
+				<option name="niveau"> Niveau</option>
+				<option name="espece">Espece</option>
+
+			</select>
+			<input type="submit" value="Envoyer"/>
+		</form>
+		<table border=2 id="tableau">
 			<thead>
-			<tr>
+			<tr class="ligne">
 				<th>Nom</th>
 				<th>Niveau</th>
 				<th>Espèce</th>
-				<th>Types</th>
+				<!--<th>Types</th>
 				<th>Taille</th>
 				<th>Poids</th>
 				<th>Légendaire</th>
 				<th>Attaque signature</th>
-				<th>PV</th>
+				<th>PV</th> !-->
 			</tr>
 			</thead>
-            <tbody>
-			<?php include("requete.php") ?>
-            </tbody>
+            		<tbody>
+				<?php include("connexion.php") ?>
+            		</tbody>
 		</table>
         <?php include("close.php"); ?>
 	</body>

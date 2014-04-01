b4205c0aeba245b361b700fdc7916fd520a52b84
William BENAISSA <E139157H@I123V2pc17.ensinfo.sciences.univ-nantes.prive>
2014-04-01 18:14:12 +0200

    Architecture normalement définitive du html
    * php/index.php: Mise en place de toutes les divs, j'ai enlevé le select !

--- php/index.php
+++ php/index.php
@@ -6,34 +6,33 @@
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-		<h1>liste de pokemons</h1>
-		<form method="post" action="">
-			<select id="tri_select">
-				<option name="nom">Nom</option>
-				<option name="niveau"> Niveau</option>
-				<option name="espece">Espece</option>
-
-			</select>
-			<input type="submit" value="Envoyer"/>
-		</form>
-		<table border=2 id="tableau">
-			<thead>
-			<tr class="ligne">
-				<th>Nom</th>
-				<th>Niveau</th>
-				<th>Espèce</th>
-				<!--<th>Types</th>
-				<th>Taille</th>
-				<th>Poids</th>
-				<th>Légendaire</th>
-				<th>Attaque signature</th>
-				<th>PV</th> !-->
-			</tr>
-			</thead>
-            		<tbody>
-				<?php include("connexion.php") ?>
-            		</tbody>
-		</table>
+		<div id="header">
+			<div id="titre">
+				<h1>liste de pokemons</h1>
+			</div>
+			<div id="recherche">
+			</div>
+		</div>
+		<div id="content">
+			<table border=2 id="tableau">
+				<thead>
+					<tr class="ligne">
+					<th>Nom</th>
+						<th>Niveau</th>
+					<th>Espèce</th>
+					<!--<th>Types</th>
+					<th>Taille</th>
+					<th>Poids</th>
+					<th>Légendaire</th>
+					<th>Attaque signature</th>
+					<th>PV</th> !-->
+				</tr>
+				</thead>
+            			<tbody>
+					<?php include("connexion.php") ?>
+            			</tbody>
+			</table>
+		</div>
         <?php include("close.php"); ?>
 	</body>
 </html>

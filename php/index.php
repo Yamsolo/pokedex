570178102a30ddbf709d252f940d98b553bcdd8c
Pierre-Antoine RAULT <E134894Z@I123V2pc09.ensinfo.sciences.univ-nantes.prive>
2014-04-08 17:48:32 +0200

    Changed css for index.php forms: new pokemon fonts,better background integration.
    * css/main.css: changed #recherche, added class .recherche (for pokemon fonts)
    
    * php/index.php: corresponding update.
    
    * css/(new ttf fonts)

--- php/index.php
+++ php/index.php
@@ -6,15 +6,15 @@
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-	  <?php include("connexion.php") ?>
 		<div id="header">
 			<div id="titre">
-				<h1>PC de Léo</h1>
+				<h1 class="recherche">PC de Léo</h1>
 			</div>
 			<div id="recherche">
+				<p class="recherche">Rechercher un pokémon</p>
 				<form id="search" method="post">
-					<input class="barre" name="name" type="text" placeholder="nom du pokémon" required /></br>
-					<input class="barre" name="type" type="text" placeholder="nom du type" required /></br>
+					<input class="barre" name="name" type="text" placeholder="nom du pokémon" /></br>
+					<input class="barre" name="type" type="text" placeholder="nom du type" /></br>
 					<input class="bouton" type="submit" value="rechercher" />
 				</form>
 			</div>
@@ -35,7 +35,7 @@
 				</tr>
 				</thead>
             			<tbody>
-				  <?php include("requete.php"); ?>
+					<?php include("connexion.php") ?>
             			</tbody>
 			</table>
 		</div>

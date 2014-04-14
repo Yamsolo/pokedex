f5b6e0afc79f34d83404b1c1aefd2b80380596b3
William Benaissa <yamsolo49@gmail.com>
2014-04-14 19:13:03 +0200

    Create fonction_table.php

---
+++ php/fonction_table.php
@@ -0,0 +1,44 @@
+<?php
+	include("../config.php");
+	$link = mysql_connect($connect, $login, $password) or die("Impossible de se connecter à la base");
+	mysql_select_db($database, $link) or die("Impossible de selectionner la base de donnée");
+	mysql_set_charset("utf8", $link);
+/*
+	function ajout_pokemon($nom, $niveau, $espece, $type, $taille, $poids, $legendaire, $AS, $PV)
+	{
+		$request = mysql_query("SELECT * FROM Especes WHERE Espece");
+		$idEsp = "";
+		$idType = "";
+		while ($tab = mysql_fetch_array($request))
+			if (strstr(strtoupper($tab['Espece']), strtoupper($espece)))
+				$idEsp = $tab['idEsp'];
+		if ($idEsp == "")
+		{
+			mysql_query("INSERT INTO Especes (Espece) VALUES ($espece)");
+			while ($tab = mysql_fetch_array($request))
+				if (strstr(strtoupper($tab['Espece']), strtoupper($espece)))
+					$idEsp = $tab['idEsp'];
+		}
+		$request = mysql_query("SELECT * FROM Types");
+			while ($tab = mysql_fetch_array($request))
+				if (strstr(strtoupper($tab['PokeType']), strtoupper($type)))
+					$type_name = $tab['PokeType'];
+			if ($type_name == "")
+			mysql_query("INSERT INTO Type (PokeType) VALUES ($type)");		
+		if ($idEsp == "")
+		{
+			mysql_query("INSERT INTO Especes (Espece) VALUES ($espece)");
+			while ($tab = mysql_fetch_array($request))
+				if (strstr(strtoupper($tab['Espece']), strtoupper($espece)))
+					$idEsp = $tab['idEsp'];
+		}
+		//echo "INSERT INTO Pokedex (Nom, Taille, Poids, Legendaire, AttaqueSpe, PV, idEsp) 
+		//				VALUES (".$nom.", ".$taille.",".$poids.",".$legendaire.",".$AS.",".$PV.", ".$idEsp.")";
+}
+*/
+function delete_pokemon($id)
+{
+	$request = "DELETE FROM Pokemon WHERE idNumero = $id";
+	mysql_query($request) or die("Erreur lors de la supression du Pokemon");
+}
+?>
